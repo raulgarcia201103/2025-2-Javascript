8376b2e564208783a652422a53ceee4a8ab76399
raulgarcia201103 <raul_antonio_gar@hotmail.com>
2025-05-12 20:50:11 -0600

    Resuelvo tarea de la clase X

--- semana1/clase4_tarea.js
+++ semana1/clase4_tarea.js
@@ -7,9 +7,71 @@
 
 const { ask } = require('../helpers/input');
 
+// Función para saber los días
+function diffDays(actualDate, birthDate) {
+    const diff = actualDate - birthDate;
+    return Math.floor(diff / (1000 * 60 * 60 * 24));
+}
+
+// Función para saber los meses
+function diffMonths(actualDate, birthDate) {
+    const totalAños = actualDate.totalDeAños() - birthDate.totalDeAños();
+    const totalMeses = actualDate.totalDeMeses() - birthDate.totalDeMeses();
+    const totalMonths = totalAños * 12 + totalMeses;
+
+    return totalMonths;
+}
+
+// Función para saber los años
+function diffYears(actualDate, birthDate) {
+    let totalAños = actualDate.totalDeAños() - birthDate.totalDeAños();
+
+    if (
+        actualDate.getMonth() < birthDate.getMonth() ||
+        (actualDate.getMonth() === birthDate.getMonth() && actualDate.getDate() < birthDate.getDate())
+    ) {
+        totalAños--;
+    }
+
+    return totalAños;
+}
+
+// Este no le entendi muy bien pero fuciono, hay que preguntar 
+Date.prototype.totalDeAños = function () {
+    return this.getFullYear();
+};
+
+Date.prototype.totalDeMeses = function () {
+    return this.getMonth();
+};
+
 async function main() {
+    const actualDate = new Date();
+    
+    const birthDateDay = await ask('¿Cuál es el día de tu fecha de cumpleaños?');
+    const birthDateMonth = await ask('¿Cuál es el mes de tu fecha de cumpleaños?');
+    const birthDateYear = await ask('¿Cuál es el año de tu fecha de cumpleaños?');
 
+    const birthDate = new Date(birthDateYear, birthDateMonth - 1, birthDateDay);
 
+    console.log('Has vivido aproximadamente:');
+    console.log(`${diffDays(actualDate, birthDate)} días`);
+    console.log(`${diffMonths(actualDate, birthDate)} meses`);
+    console.log(`${diffYears(actualDate, birthDate)} años`);
+
+    // Calculamos la edad
+    const edad = diffYears(actualDate, birthDate);  // Aquí se define 'edad'
+
+    // Mensajes según la edad
+    if (edad <= 18) {
+        console.log('Eres un menor de edad');
+    } else if (edad > 18 && edad <= 30) {
+        console.log('Eres un adulto joven');
+    } else if (edad > 30 && edad <= 50) {
+        console.log('Ya ve haciendo tu testamento');
+    } else {
+        console.log('Ya estás robando oxígeno :(');
+    }
 }
 
 main();
